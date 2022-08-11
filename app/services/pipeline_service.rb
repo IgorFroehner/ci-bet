6f1473bdd7c98c5e31f1877ba491bb12121df999
EMachad0 <elitonmachadod200@gmail.com>
2022-08-11 14:20:16 -0300

    update pipeline_service.rb

--- app/services/pipeline_service.rb
+++ app/services/pipeline_service.rb
@@ -1,38 +1,39 @@
 # frozen_string_literal: true
 
 class PipelineService
-  def get_all_pipelines
-    url = URI("#{CIRCLE_API_URL}/project/#{PROJECT_SLUG}/pipeline")
-    send_request(url)
-  end
+  class << self
+    def get_all_pipelines
+      url = URI("#{CIRCLE_API_URL}/project/#{PROJECT_SLUG}/pipeline")
+      send_request(url)
+    end
 
-  def get_pipeline_by_number(pipeline_number)
-    url = URI("#{CIRCLE_API_URL}/project/#{PROJECT_SLUG}/pipeline/#{pipeline_number}")
-    send_request(url)
-  end
+    def get_latest_pipeline
+      get_all_pipelines['items'].first
+    end
 
-  def get_latest_pipeline
-    url = URI("#{CIRCLE_API_URL}/project/#{PROJECT_SLUG}/pipeline")
-    send_request(url)[0]
-  end
+    def get_pipeline_by_number(pipeline_number)
+      url = URI("#{CIRCLE_API_URL}/project/#{PROJECT_SLUG}/pipeline/#{pipeline_number}")
+      send_request(url)
+    end
 
-  private
+    private
 
-  CIRCLE_API_URL = 'https://circleci.com/api/v2/'
-  PROJECT_SLUG = 'gh/trusted/trusted-api'
+    CIRCLE_API_URL = 'https://circleci.com/api/v2'
+    PROJECT_SLUG = 'gh/trusted/trusted-api'
 
-  CIRCLE_TOKEN = ENV.fetch('CIRCLE_CI_API_TOKEN')
+    CIRCLE_TOKEN = ENV.fetch('CIRCLE_CI_API_TOKEN')
 
-  def send_request(url)
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    def send_request(url)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
-    request['Circle-Token'] = CIRCLE_TOKEN
-    request['Accept'] = 'text/plain'
+      request = Net::HTTP::Get.new(url)
+      request['Circle-Token'] = CIRCLE_TOKEN
+      request['Accept'] = 'text/plain'
 
-    response = http.request(request)
-    response.read_body
+      response = http.request(request)
+      JSON.parse(response.read_body)
+    end
   end
 end
