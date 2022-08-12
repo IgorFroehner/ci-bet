2efecb39df92c757117e89374e4649440179b9f8
EMachad0 <elitonmachadod200@gmail.com>
2022-08-12 11:32:34 -0300

    scripts

--- app/scripts/end_game_script.rb
+++ app/scripts/end_game_script.rb
@@ -1,4 +1,3 @@
-
 class EndGameScript
   def self.run
     puts 'EndGameScript running, use Ctrl-C to stop.'
@@ -19,7 +18,7 @@ class EndGameScript
       end
 
       unless finished_status.include?(status)
-        puts 'Game still running, waiting for it to finish.'
+        Rails.logger.info('Game still running, waiting for it to finish.')
         sleep(10)
         next
       end
@@ -35,14 +34,16 @@ class EndGameScript
       )
 
       if status == 'success' || status == 'failed'
-        winners = game.end_game(status == 'success')
-        text = winners.map { |entry, balance| "#{entry['user_name']} +#{balance}#{balance >= 10000 ? " #{money_emoji}" : ''}" }.join('\n')
+        entries = game.end_game(status == 'success')
+        parts = entries
+                  .map { |entry, balance| change_parts(entry, balance) }
+                  .flatten
 
-        unless text.blank?
+        unless parts.blank?
           slack_client.chat_postMessage(
             channel: ENV['CHANNEL_ID'],
             thread_ts: message_result['ts'],
-            text: text
+            blocks: change_blocks(parts)
           )
         end
       end
@@ -53,63 +54,83 @@ class EndGameScript
     end
   end
 
-  def self.message(pipeline, status)
-    [
-      {
-        "type": "header",
-        "text": {
-          "type": "plain_text",
-          "text": "#{emoji(status)} Game Finished #{emoji(status)}",
-          "emoji": true
+  class << self
+    def message(pipeline, status)
+      [
+        {
+          "type": "header",
+          "text": {
+            "type": "plain_text",
+            "text": "#{emoji(status)} Game Finished #{emoji(status)}",
+            "emoji": true
+          }
+        }, {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Commit:* #{pipeline['vcs']['commit']['subject']}\n*By:* #{pipeline['trigger']['actor']['login']}\n*Status:* #{status} :money-money:\n"
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": image(status),
+            "alt_text": "circle ci image"
+          }
+        }, {
+          "type": "divider"
+        }, {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Distributing gains!!! :burning-money: Next game will start soon... :flying_money_with_wings:"
+          }
         }
-      }, {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "*Commit:* #{pipeline['vcs']['commit']['subject']}\n*By:* #{pipeline['trigger']['actor']['login']}\n*Status:* #{status} :money-money:\n"
-        },
-        "accessory": {
-          "type": "image",
-          "image_url": image(status),
-          "alt_text": "circle ci image"
-        }
-      }, {
-        "type": "divider"
-      }, {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Distributing gains!!! :burning-money: Next game will start soon... :flying_money_with_wings:"
-        }
-      }
-    ]
-  end
-
-  def self.money_emoji
-    %w[:burning-money: :flying_money_with_wings: :rocket_intensifies: :catjam:].sample
-  end
-
-  def self.emoji(status)
-    if status == 'success'
-      ':happy-link:'
-    elsif status == 'failed'
-      ':rocket_intensifies:'
-    else
-      ':catjam:'
+      ]
     end
-  end
 
-  def self.finished_status
-    %w[success failed canceled]
-  end
+    def self.money_emoji
+      %w[:burning-money: :flying_money_with_wings: :rocket_intensifies: :catjam:].sample
+    end
 
-  def self.image(status)
-    if status == 'success'
-      'https://i.ibb.co/fxMdzkH/circle-ci-trademark-sucess-c.png'
-    elsif status == 'failed'
-      'https://i.ibb.co/m6JQXjY/circle-ci-trademark-failed-c.png'
-    else
-      'https://cdn.iconscout.com/icon/free/png-256/circleci-283066.png'
+    def emoji(status)
+      if status == 'success'
+        ':happy-link:'
+      elsif status == 'failed'
+        ':rocket_intensifies:'
+      else
+        ':catjam:'
+      end
+    end
+
+    def finished_status
+      %w[success failed canceled]
+    end
+
+    def image(status)
+      if status == 'success'
+        'https://i.ibb.co/fxMdzkH/circle-ci-trademark-sucess-c.png'
+      elsif status == 'failed'
+        'https://i.ibb.co/m6JQXjY/circle-ci-trademark-failed-c.png'
+      else
+        'https://cdn.iconscout.com/icon/free/png-256/circleci-283066.png'
+      end
+    end
+
+    def change_parts(entry, amount)
+      name = entry['user_name'].split('.').map(&:capitalize).join(" ")
+      amount = "#{amount > 0 ? '+' : ''}#{amount}#{amount >= 1000 ? " #{money_emoji}" : ''}"
+      [
+        {
+          "type": "mrkdwn",
+          "text": "*#{name}*: #{amount}",
+        },
+      ]
+    end
+
+    def change_blocks(parts)
+      [{
+         "type": "section",
+         "fields": parts
+       }]
     end
   end
 end
